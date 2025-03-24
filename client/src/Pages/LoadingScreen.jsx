a1c4a78fe99a09798bb5746b00d41c6e182127c6
Luizcs2 <luizohto2012@gmail.com>
2025-03-24 00:27:42 +0000

    Questionnaire internship project

---
+++ client/src/Pages/LoadingScreen.jsx
@@ -0,0 +1,13 @@
+import "./CSS/LoadingScreen.css";
+import React from "react";
+import loadingVideo from "../assets/loading-screen.mp4";
+
+export default function LoadingScreen() {
+  return (
+    <div className="loading-screen">
+      <video autoPlay muted>
+        <source src={loadingVideo} type="video/mp4" />
+      </video>
+    </div>
+  );
+}
